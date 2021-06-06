ab56368c70ee813175d700d972edbd815e201b9a
anna leticia <annaleticiafranco31@gmail.com>
2021-06-06 03:29:43 -0300

    Mudando o projeto para o formato MVC

---
+++ core/database/QueryBuilder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Core\Database;
+
+use PDO;
+
+class QueryBuilder
+{
+
+    protected $pdo;
+
+
+    public function __construct()
+    {
+    
+    }
+
+
+    public function selectAll()
+    {
+      
+    }
+
+    public function insert()
+    {
+      
+         
+    }
+    public function edit()
+    {
+      
+         
+    }
+    public function delete()
+    {
+      
+         
+    }
+    public function read()
+    {
+      
+         
+    }
+}
