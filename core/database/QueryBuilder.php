8298f319296bcd491bb9482aac13786400b90110
ArthurBonaldi <arthur.bonaldi@ice.ufjf.br>
2021-06-08 15:39:15 -0300

    Erros assustadores

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -10,14 +10,20 @@ class QueryBuilder
     protected $pdo;
 
 
-    public function __construct()
+    public function __construct(PDO $pdo)
     {
-    
+        $this->pdo = $pdo;
     }
 
 
-    public function selectAll()
+    public function selectAll($table)
     {
+        $statement = $this->pdo->prepare("select * from {$table}") ;
+
+        $statement->execute(); 
+       
+       
+        return $statement->fetchAll(PDO::FETCH_CLASS);
       
     }
 
