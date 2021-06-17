1a548f6353f712ace9dbb1209c225bd162882dec
ArthurBonaldi <arthur.bonaldi@ice.ufjf.br>
2021-06-17 10:07:50 -0300

    alterando rota

--- app/routes.php
+++ app/routes.php
@@ -2,7 +2,6 @@
     $router->get('', 'UserController@index');
     $router->get('adminUser', 'UserController@adm');
     $router->get('addUse', 'UserController@add');
-    $router->get('edit_users', 'UserController@edit');
     $router->post('adicionarUsuario', 'UserController@create');
     $router->post('deleteUser', 'UserController@delete');
     $router->post('view_user' , 'UserController@view');
