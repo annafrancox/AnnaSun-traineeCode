a504de71093aaba3f71f8398b92051e686ded3c5
miles-02 <miguelsales02@gmail.com>
2021-06-22 22:08:19 -0300

    Correcao de rotas duplicadas

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInitf5e613e64b5c0d3d0dd0b9eefbe0324a
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitf5e613e64b5c0d3d0dd0b9eefbe0324a', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitf5e613e64b5c0d3d0dd0b9eefbe0324a', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitf5e613e64b5c0d3d0dd0b9eefbe0324a::getInitializer($loader));
         } else {
