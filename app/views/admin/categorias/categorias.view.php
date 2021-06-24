27f009bac3b558581b07ccd0dfdaab9e00622d4a
isa56 <isadoraadriana@gmail.com>
2021-06-24 09:34:28 -0300

    Adicionados navbar e footer

--- app/views/admin/categorias/categorias.view.php
+++ app/views/admin/categorias/categorias.view.php
@@ -15,6 +15,9 @@
 </head>
 
 <body>
+
+<?php require('../../include/nav.php'); ?>
+
   <!-- Inicio listagem de categorias -->
   <div class="container" style="margin-bottom: 15px;">
         
@@ -55,8 +58,8 @@
 
     </div>
   <!-- Fim listagem de categorias -->
-  
 
+  <?php require('../../include/footer.php'); ?>
 
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
